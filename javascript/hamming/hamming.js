de059ce6b06fa43799eb2b0293def5c9ad55967f
imtibbet <tibbian@gmail.com>
2015-07-10 21:37:12 +0000

    working on hammond dist

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,9 @@
+function compute(t1, t2) {
+	if (t1.length() != t2.length()) { 
+		throw new Error("DNA strands must be of equal length.");
+	}
+	var dist = 0;
+	return dist;
+}
+
+module.exports = compute;
